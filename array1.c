d27224ae640f95ba034cce732566cb12d31cdad8
candy5005k <103052118+candy5005k@users.noreply.github.com>
2022-04-05 20:00:28 +0530

    Add files via upload

---
+++ array1.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+{
+	//int no = 11;
+	//int arr[2];
+	//int i;
+	
+	
+	int arr[2]={10000,10};
+	 
+	/*arr[0] = 11;
+	arr[1] = 21;
+	arr[2] = 51;
+	arr[3] = 101;
+	arr[4] = 111;*/
+	//arr[i]=0;
+	printf("%d\n",sizeof(arr));
+	printf("address of arr %u\n",arr);
+	printf("print arr value:%d\n",arr[0]);
+	//printf("",);
+	
+	
+	
+
+	return 0;
+}
